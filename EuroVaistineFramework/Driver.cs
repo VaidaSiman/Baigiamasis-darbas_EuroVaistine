f401919dcc0f1ff6ed2aac049573daa995831ac9
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-05-08 17:27:56 +0300

    Add Driver class methods

--- EuroVaistineFramework/Driver.cs
+++ EuroVaistineFramework/Driver.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace EuroVaistineFramework
 {
     internal class Driver
     {
+        private static IWebDriver driver;
+
+        internal static void InitializeDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        internal static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        internal static void OpenPage()
+        {
+            driver.Navigate().GoToUrl(string url);
+            driver.Url = url;
+        }
+
+        internal static void ShutdownDriver()
+        {
+            driver.Quit();
+        }
     }
 }
